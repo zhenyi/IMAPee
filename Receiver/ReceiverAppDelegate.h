de58e6372d74c236ff1546d53cc41c0bdda172aa
Zhenyi Tan <qqzhenyi@gmail.com>
2011-05-24 10:01:33 +0800

    Initial Commit

---
+++ Receiver/ReceiverAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ReceiverAppDelegate.h
+//  Receiver
+//
+//  Created by Zhenyi Tan on 5/24/11.
+//  Copyright 2011 And a Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiverAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Receiver/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Receiver
+//
+//  Created by Zhenyi Tan on 5/24/11.
+//  Copyright 2011 And a Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
