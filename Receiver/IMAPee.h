abc0d89e725c2bd778c964fa80cac0dbd9c50498
Zhenyi Tan <qqzhenyi@gmail.com>
2011-06-08 13:04:58 +0800

    Added encode/decode utf7 methods, and some datetime formatter.

--- Receiver/IMAPee.h
+++ Receiver/IMAPee.h
@@ -7,9 +7,67 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSString+Additions.h"
 
 @interface IMAPee : NSObject {
     
 }
+
++ (NSString *) decodeUTF7:(NSString *)aString;
++ (NSString *) encodeUTF7:(NSString *)aString;
++ (NSString *) formatDate:(NSDate *)someDate;
++ (NSString *) formatDateTime:(NSDate *)someDate;
+
+/*
+ add_authenticator
+ add_response_handler
+ append
+ authenticate
+ capability
+ check
+ close
+ copy
+ create
+ debug
+ debug=
+ delete
+ disconnect
+ disconnected?
+ examine
+ expunge
+ fetch
+ getacl
+ getquota
+ getquotaroot
+ idle
+ idle_done
+ list
+ login
+ logout
+ lsub
+ max_flag_count
+ max_flag_count=
+ new
+ noop
+ remove_response_handler
+ rename
+ search
+ select
+ setacl
+ setquota
+ sort
+ starttls
+ status
+ store
+ subscribe
+ thread
+ uid_copy
+ uid_fetch
+ uid_search
+ uid_sort
+ uid_store
+ uid_thread
+ unsubscribe
+*/
 
 @end
--- Receiver/IMAPee.m
+++ Receiver/IMAPee.m
@@ -10,4 +10,78 @@
 
 @implementation IMAPee
 
++ (NSString *) decodeUTF7:(NSString *)aString {
+    NSError *error = NULL;
+    NSRegularExpression *decodeUTF7Regex = [NSRegularExpression regularExpressionWithPattern:@"&(.*?)-"
+                                                                                     options:0
+                                                                                       error:&error];
+    NSTextCheckingResult *match = [decodeUTF7Regex firstMatchInString:aString options:0 range:NSMakeRange(0, [aString length])];
+    NSString *matchedString = nil;
+    if (match) {
+        NSRange matchedRange = [match rangeAtIndex:1];
+        if (!NSEqualRanges(matchedRange, NSMakeRange(NSNotFound, 0))) {
+            matchedString = [aString substringWithRange:matchedRange];
+        }
+        if ([matchedString length] == 0) {
+            return @"&";
+        } else {
+            NSString *base64 = [matchedString stringByReplacingOccurrencesOfString:@"," withString:@"/"];
+            int x = [base64 length] % 4;
+            if (x > 0) {
+                base64 = [base64 stringByPaddingToLength:([base64 length] + 4 - x) withString:@"=" startingAtIndex:0];
+            }
+            NSString *decoded = [NSString stringFromBase64String:base64];
+            NSString *u16tou8 = [NSString stringWithCString:[decoded cStringUsingEncoding:NSUTF8StringEncoding] encoding:NSUTF16BigEndianStringEncoding];
+            return u16tou8;
+        }
+    } else {
+        return aString;
+    }
+}
+
++ (NSString *) encodeUTF7:(NSString *)aString {
+    NSError *error = NULL;
+    NSRegularExpression *encodeUTF7Regex = [NSRegularExpression regularExpressionWithPattern:@"(&)|([^\\x20-\\x25\\x27-\\x7e]+)"
+                                                                                     options:0
+                                                                                       error:&error];
+    NSTextCheckingResult *match = [encodeUTF7Regex firstMatchInString:aString options:0 range:NSMakeRange(0, [aString length])];
+    NSString *ampersand = nil;
+    if (match) {
+        NSRange ampersandRange = [match rangeAtIndex:1];
+        if (!NSEqualRanges(ampersandRange, NSMakeRange(NSNotFound, 0))) {
+            ampersand = [aString substringWithRange:ampersandRange];
+        }
+        if (ampersand) {
+            return @"&-";
+        } else {
+            NSString *matchedString = [aString substringWithRange:[match range]];
+            NSString *u8tou16 = [NSString stringWithCString:[matchedString cStringUsingEncoding:NSUTF16BigEndianStringEncoding]
+                                                   encoding:NSUTF8StringEncoding];
+            NSString *base64 = [NSString base64StringFromString:u8tou16];
+            NSString *stringWithoutEquals = [base64 stringByReplacingOccurrencesOfString:@"=" withString:@""];
+            NSString *stringWithoutEqualsAndNewLines = [stringWithoutEquals stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            NSString *utf7 = [stringWithoutEqualsAndNewLines stringByReplacingOccurrencesOfString:@"/" withString:@","];
+            return [NSString stringWithFormat:@"&%@-", utf7];
+        }
+    } else {
+        return aString;
+    }
+}
+
++ (NSString *) formatDate:(NSDate *)someDate {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd-MMM-yyyy"];
+    NSString *formatted = [dateFormatter stringFromDate:someDate];
+    [dateFormatter release];
+    return formatted;
+}
+
++ (NSString *) formatDateTime:(NSDate *)someDate {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd-MMM-yyyy HH:mm Z"];
+    NSString *formatted = [dateFormatter stringFromDate:someDate];
+    [dateFormatter release];
+    return formatted;
+}
+
 @end
